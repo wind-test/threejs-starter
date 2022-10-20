358d4eab7f0a5fd4f2c1a9857563d774487e71e3
huangjitao <164173925@qq.com>
2022-10-20 16:33:14 +0800

    feat: 银河系粒子生成器

--- src/pages/chapter6-3/index.tsx
+++ src/pages/chapter6-3/index.tsx
@@ -10,20 +10,26 @@ import { useSize } from "ahooks";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import GUI from "lil-gui";
 
 let scene: THREE.Scene,
   renderer: THREE.WebGLRenderer,
   camera: THREE.PerspectiveCamera,
-  controls: OrbitControls
+  controls: OrbitControls,
+  points: THREE.Points<THREE.BufferGeometry, THREE.PointsMaterial>,
+  geometry: THREE.BufferGeometry,
+  material: THREE.PointsMaterial
 
 const params = {
-  count: 10000,
-  size: 0.1,
-  radius: 5,
-  branch: 3,
-  color: "#ff6030",
-  rotateScale: 0.3,
-  endColor: "#1b3984",
+  count: 1000, // 粒子个数
+  size: 0.02,  // 粒子大小
+  radius: 5,  // 银河系半径
+  branch: 3,  // 银河系分支书
+  spin: 1, // 偏转角度
+  randomness: 0.2,  // 随机扩散
+  randomnessPower: 3, // 随机扩散幂
+  insideColor: '#ff6030',
+  outsideColor: '#1b3984',
 };
 
 
@@ -32,81 +38,74 @@ const Chapter6_3 = () => {
   const size = useSize(ref);
 
   const generateGalaxy = () => {
-    const textureLoader = new THREE.TextureLoader();
-    const particlesTexture = textureLoader.load("/textures/particles/1.png");
-    const centerColor = new THREE.Color(params.color);
-    const endColor = new THREE.Color(params.endColor);
-    // 生成顶点
-    const geometry = new THREE.BufferGeometry();
-    //   随机生成位置集合
-    const positions = new Float32Array(params.count * 3);
-    // 设置顶点颜色
-    const colors = new Float32Array(params.count * 3);
-
-    //   循环生成点
-    for (let i = 0; i < params.count; i++) {
-      //   当前的点应该在哪一条分支的角度上
-      const branchAngel = (i % params.branch) * ((2 * Math.PI) / params.branch);
-
-      // 当前点距离圆心的距离
-      const distance = Math.random() * params.radius * Math.pow(Math.random(), 3);
-      const current = i * 3;
-
-      const randomX =
-        (Math.pow(Math.random() * 2 - 1, 3) * (params.radius - distance)) / 5;
-      const randomY =
-        (Math.pow(Math.random() * 2 - 1, 3) * (params.radius - distance)) / 5;
-      const randomZ =
-        (Math.pow(Math.random() * 2 - 1, 3) * (params.radius - distance)) / 5;
-
-      positions[current] =
-        Math.cos(branchAngel + distance * params.rotateScale) * distance +
-        randomX;
-      positions[current + 1] = 0 + randomY;
-      positions[current + 2] =
-        Math.sin(branchAngel + distance * params.rotateScale) * distance +
-        randomZ;
-
-      // 混合颜色，形成渐变色
-      const mixColor = centerColor.clone();
-      mixColor.lerp(endColor, distance / params.radius);
-
-      colors[current] = mixColor.r;
-      colors[current + 1] = mixColor.g;
-      colors[current + 2] = mixColor.b;
+    if (points) {
+      geometry.dispose()
+      material.dispose()
+      scene.remove(points)
     }
 
+    geometry = new THREE.BufferGeometry()
+    const positions = new Float32Array(params.count * 3)
+    const colors = new Float32Array(params.count * 3)
+    for (let i = 0; i < params.count; i++) {
+      const radius = Math.random() * params.radius;
+      // 根据所在的分支来获取坐标上的角度
+      const branchesAngle = ((i % params.branch) / params.branch) * Math.PI * 2;
+      // 设置偏转角度，将直线变成曲线
+      const spinAngle = radius * params.spin;
+
+      // 设置粒子颜色
+      const colorInside = new THREE.Color(params.insideColor)
+      const colorOutside = new THREE.Color(params.outsideColor)
+      const mixedColor = colorInside.clone()
+      mixedColor.lerp(colorOutside, radius / params.radius)
+      colors[i * 3] = mixedColor.r
+      colors[i * 3 + 1] = mixedColor.g
+      colors[i * 3 + 2] = mixedColor.b
+      // 设置粒子的随机扩散
+      const randomX = Math.random() ** params.randomnessPower
+        * (Math.random() < 0.5 ? 1 : -1)
+        * params.randomness
+        * radius
+      const randomY = Math.random() ** params.randomnessPower
+        * (Math.random() < 0.5 ? 1 : -1)
+        * params.randomness
+        * radius
+      const randomZ = Math.random() ** params.randomnessPower
+        * (Math.random() < 0.5 ? 1 : -1)
+        * params.randomness
+        * radius
+
+      positions[i * 3] = Math.cos(branchesAngle + spinAngle) * radius + randomX;
+      positions[i * 3 + 1] = randomY;
+      positions[i * 3 + 2] = Math.sin(branchesAngle + spinAngle) * radius + randomZ;
+    }
     geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
     geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 
-    //   设置点材质
-    const material = new THREE.PointsMaterial({
-      // color: new THREE.Color(params.color),
+    material = new THREE.PointsMaterial({
       size: params.size,
       sizeAttenuation: true,
       depthWrite: false,
       blending: THREE.AdditiveBlending,
-      map: particlesTexture,
-      alphaMap: particlesTexture,
-      transparent: true,
       vertexColors: true,
     });
 
-    const points = new THREE.Points(geometry, material);
-    return points
+    points = new THREE.Points(geometry, material);
+    scene.add(points);
   };
 
   const initThree = () => {
     /** --- 创建一个场景 --- */
     scene = new THREE.Scene();
 
+    // 银河系粒子生成
+    generateGalaxy();
+
     /** --- 设置光源 --- */
     // 环境光
     const ambient = new THREE.AmbientLight(0xffffff, 0.5);
     scene.add(ambient);
-
-    const points = generateGalaxy()
-    scene.add(points)
 
     //直线光源
     const pointLight = new THREE.PointLight(0xff0000, 1);
@@ -123,10 +122,10 @@ const Chapter6_3 = () => {
       75,
       window.innerWidth / window.innerHeight,
       0.1,
-      1000
+      100
     );
     // 设置相机位置
-    camera.position.set(0, 0, 10);
+    camera.position.set(4, 1.8, 4)
     //设置相机方向(指向的场景对象)
     camera.lookAt(scene.position);
 
@@ -140,6 +139,22 @@ const Chapter6_3 = () => {
 
     /** ---创建轨道控制器--- */
     controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true
+    controls.zoomSpeed = 0.3
+
+    const gui = new GUI()
+    gui.add(controls, 'autoRotate')
+    gui.add(controls, 'autoRotateSpeed', 0.1, 10, 0.01)
+    gui.add(params, 'count', 100, 100000, 100).onFinishChange(generateGalaxy)
+    gui.add(params, 'size', 0.001, 0.1, 0.001).onFinishChange(generateGalaxy)
+    gui.add(params, 'radius', 0.01, 20, 0.01).onFinishChange(generateGalaxy)
+    gui.add(params, 'branch', 2, 20, 1).onFinishChange(generateGalaxy)
+    gui.add(params, 'spin', -5, 5, 0.001).onFinishChange(generateGalaxy)
+    gui.add(params, 'randomness', 0, 2, 0.001).onFinishChange(generateGalaxy)
+    gui.add(params, 'randomnessPower', 1, 10, 0.001).onFinishChange(generateGalaxy)
+    gui.addColor(params, 'insideColor').onFinishChange(generateGalaxy)
+    gui.addColor(params, 'outsideColor').onFinishChange(generateGalaxy)
+    gui.close()
 
     render();
   };
